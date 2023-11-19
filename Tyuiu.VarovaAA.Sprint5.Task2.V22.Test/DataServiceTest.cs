716789cc54b07f34addeb30c1a66c00bcae85c3a
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-11-19 22:26:53 +0500

    Task2

---
+++ Tyuiu.VarovaAA.Sprint5.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.VarovaAA.Sprint5.Task2.V22.Lib;
+namespace Tyuiu.VarovaAA.Sprint5.Task2.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.VarovaAA.Sprint5\Tyuiu.VarovaAA.Sprint5.Task2.V22\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
